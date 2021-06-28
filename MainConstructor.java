6dd24e84b5681350f693f4a5d67f06e41477b438
Aparna singh <75516079+singh99aparna@users.noreply.github.com>
2021-06-28 11:54:41 -0700

    Add files via upload

---
+++ MainConstructor.java
@@ -0,0 +1,21 @@
+class Constructor
+{
+	Constructor()
+	 {
+		int a=10;
+		int b=50;
+		int c;
+		c=a+b;
+		System.out.println("DEFAULT CONTRUCTOR");
+		System.out.println("Total is :"+c);
+	 }
+}
+
+public class MainConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Constructor con= new Constructor();
+	}
+
+}
