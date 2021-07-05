8cfe5fe5955eda796edf91666068d7580df4a880
Aparna singh <75516079+singh99aparna@users.noreply.github.com>
2021-07-05 11:15:03 -0700

    Add files via upload

---
+++ SimpleObject.java
@@ -0,0 +1,13 @@
+public class SimpleObject {
+SimpleObject()
+{
+	System.out.println("HELLO THIS IS CONSTRUCTOR");
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SimpleObject so= new SimpleObject();
+	}
+
+}
+
